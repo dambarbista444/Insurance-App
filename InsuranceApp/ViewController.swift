72e6719475e3c945e12796da9f6d771cc7e9e550
Mohsin Jamadar <mjamadar@virtusa.com>
2021-08-10 12:49:48 -0400

    Added pods with example

--- InsuranceApp/ViewController.swift
+++ InsuranceApp/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import SnapKit
+import PromiseKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    lazy var box = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        testPods()
     }
 
+    func testPods() {
+        // SnapKit
+        self.view.addSubview(box)
+        box.backgroundColor = .blue
+        box.snp.makeConstraints { (make) -> Void in
+            make.width.height.equalTo(50)
+            make.center.equalTo(self.view)
+        }
+        
+        // Alamofire, PromiseKit, SwiftyJSON
+        getUserInfo()
+            .done { json -> Void in
+                // Do something with the JSON info
+                let value = JSON(json)
+                debugPrint(value.dictionaryValue)
+            }
+            .catch { error in
+                // Handle error or give feedback to the user
+                debugPrint(error.localizedDescription)
+            }
+    }
+}
 
+extension ViewController {
+    func getUserInfo() -> Promise<[String: Any]> {
+        return Promise { seal in
+            AF.request("https://jsonplaceholder.typicode.com/users/1")
+                .validate()
+                .responseJSON { response in
+                    
+                    switch response.result {
+                    case .success(let json):
+                        guard let json = json  as? [String: Any] else {
+                            return seal.reject(AFError.responseValidationFailed(reason: .dataFileNil))
+                        }
+                        seal.fulfill(json)
+                    case .failure(let error):
+                        seal.reject(error)
+                    }
+                }
+        }
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+
+platform :ios, '14.0'
+use_frameworks!
+
+target "InsuranceApp" do
+  pod "PromiseKit", "~> 6.8"
+  pod 'Alamofire', '~> 5.2'
+  pod 'SwiftyJSON', '~> 4.0'
+  pod 'SnapKit', '~> 5.0.0'
+end
